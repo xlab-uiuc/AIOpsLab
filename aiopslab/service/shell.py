b977e52620d7cf9d597fd690e3ab176b6cae79c5
Jackson Clark <jclark58@uic.edu>
2025-03-06 13:09:17 -0600

    properly escape quotes so commands with json will work.

--- aiopslab/service/shell.py
+++ aiopslab/service/shell.py
@@ -22,7 +22,6 @@ class Shell:
         k8s_host = config.get("k8s_host", "localhost")  # Default to localhost
 
         if k8s_host == "kind":
-            print("[INFO] Running command inside kind-control-plane Docker container.")
             return Shell.docker_exec("kind-control-plane", command)
 
         elif k8s_host == "localhost":
@@ -95,7 +94,9 @@ class Shell:
     @staticmethod
     def docker_exec(container_name: str, command: str):
         """Execute a command inside a running Docker container."""
-        docker_command = f"docker exec {container_name} sh -c '{command}'"
+        escaped_command = command.replace('"', '\\"')
+        
+        docker_command = f'docker exec {container_name} sh -c "{escaped_command}"'
 
         try:
             out = subprocess.run(
