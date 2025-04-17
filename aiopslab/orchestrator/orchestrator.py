0dca58ae7db25c4aee7ea5ae171252b252404db7
Trunway <chuanwei@usc.edu>
2025-04-17 00:47:46 -0700

    Recover fault before users catch exceptions
    Users can catch exceptions and make program not exit. In this case, functions registered with atexit will not work to recover fault. We catch exceptions before users and recover fault in "except" part to solve this problem.

--- aiopslab/orchestrator/orchestrator.py
+++ aiopslab/orchestrator/orchestrator.py
@@ -155,19 +155,29 @@ class Orchestrator:
         action, env_response, results = "", "", {}
         self.session.start()
 
-        for step in range(max_steps):
-            action = await self.ask_agent(action_instr)
-            self.sprint.agent(action)
+        # catch any exception and recover fault before the users catch it
+        try:
+            for step in range(max_steps):
+                action = await self.ask_agent(action_instr)
+                self.sprint.agent(action)
 
-            env_response = await self.ask_env(action)
-            self.sprint.service(env_response)
+                env_response = await self.ask_env(action)
+                self.sprint.service(env_response)
 
-            if env_response == SubmissionStatus.VALID_SUBMISSION:
-                break
-            elif env_response == SubmissionStatus.INVALID_SUBMISSION:
-                raise ValueError("Invalid submission!")  # TODO (@manish): ask to retry?
+                if env_response == SubmissionStatus.VALID_SUBMISSION:
+                    break
+                elif env_response == SubmissionStatus.INVALID_SUBMISSION:
+                    raise ValueError("Invalid submission!")  # TODO (@manish): ask to retry?
 
-            action_instr = env_response + "\n" + "Please take the next action"
+                action_instr = env_response + "\n" + "Please take the next action"
+        except Exception as e:
+            # Make sure the fault cleanup function is unregistered
+            # after recovering fault ahead because of exceptions
+            with CriticalSection():
+                print("Some exception happened. Recovering the injected fault...")
+                self.session.problem.recover_fault()
+                atexit.unregister(exit_cleanup_fault)
+            raise e
 
         self.session.end()
 
